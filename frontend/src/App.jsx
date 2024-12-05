19f6ffba2d42636e82d9b1fbf53ab7ccc6470edd
Aprendiz <dreamnotfound198@gmail.com>
2024-12-05 14:55:06 -0500

    app actualizado

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,33 @@
 import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 import InicioAdmin from './Paginas/Administrador/InicioAdmin';
+import Home from './components/Home/Home';
+import Certificados from './components/Certificados/Certificados';
+import ListaCampañas from './components/ListaCampañas/ListaCampañas';
+import InfoCampañas from './components/InfoCampañas/InfoCampañas';
+import Postularse from './components/Postularse/Postularse';
+import Perfil from './components/Perfil/Perfil';
+import BuscarCampañas from './components/BuscarCampañas/BuscarCampañas';
+import GestionarEstudiantes from './components/GestionarEstudiantes/GestionarEstudiantes';
+import GestionarCampañas from './components/GestionarCampañas/GestionarCampañas';
+
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#1976d2',
+    },
+    secondary: {
+      main: '#ff4081',
+    },
+  },
+});
 function App() {
 
 
   return (
+
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<InicioAdmin />} />
