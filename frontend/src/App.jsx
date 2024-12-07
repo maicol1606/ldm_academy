eec26ded8bc4bac75a37e5e81b752a57beb82353
maicol1606 <rramirezeestacio2006@gmail.com>
2024-12-07 12:52:38 -0500

    app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,12 @@ import Home from './Paginas/default/Home';
 import Registar from './Paginas/default/registar';
 
 
+import './components/HomePage/css/style.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './components/HomePage/HomePage.jsx';
+import GenericPage from './components/GenericPage/GenericPage.jsx';
+import AuthPage from './components/AuthPage/AuthPage.jsx'; // Componente de autenticación
+
 //Importar las ruras de las paginas
 const Certificados = lazy(() => import('./Paginas/estudiante/Certificados'));
 
@@ -23,6 +29,10 @@ function App() {
         <Route exact path='/home' element={<Home/>} />
         <Route exact path='/nav' element={<NavegacionAdmin/>} />
         <Route exact path='/registar' element={<Registar/>} />
+
+        <Route path="/" element={<HomePage />} />
+        <Route path="/generic" element={<GenericPage />} />
+        <Route path="/auth" element={<AuthPage />} /> {/* Ruta para la página de autenticación */}
 
       </Routes>
     </BrowserRouter>
