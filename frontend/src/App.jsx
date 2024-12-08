4aa9bbcd8ab98a41ee87eefaecce1ebb35af72b5
loren1406 <163200705+loren1406@users.noreply.github.com>
2024-12-07 22:36:37 -0500

    .

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React,{ useState } from 'react'
+import React, { useState } from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import InicioAdmin from './Paginas/Administrador/InicioAdmin';
 import AdminHome from './Paginas/Administrador/AdminHome';
@@ -13,6 +13,7 @@ import HomePage from './components/HomePage/HomePage.jsx';
 import GenericPage from './components/GenericPage/GenericPage.jsx';
 import AuthPage from './components/AuthPage/AuthPage.jsx'; // Componente de autenticación
 
+
 //Importar las ruras de las paginas
 const Certificados = lazy(() => import('./Paginas/estudiante/Certificados'));
 
@@ -26,9 +27,9 @@ function App() {
 
         <Route path='/admin/home' exact element={<InicioAdmin />} />
         <Route path='/admin' exact element={<AdminHome />} />
-        <Route exact path='/home' element={<Home/>} />
-        <Route exact path='/nav' element={<NavegacionAdmin/>} />
-        <Route exact path='/registar' element={<Registar/>} />
+        <Route exact path='/home' element={<Home />} />
+        <Route exact path='/nav' element={<NavegacionAdmin />} />
+        <Route exact path='/registar' element={<Registar />} />
 
         <Route path="/" element={<HomePage />} />
         <Route path="/generic" element={<GenericPage />} />
